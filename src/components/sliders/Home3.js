ddd543895bc1d8c5462ebbc98d8e30a9f7c055d2
Manikanta Vedula <manikantavedula7@gmail.com>
2024-01-10 14:05:55 +0530

    new layout draft 2

--- src/components/sliders/Home3.js
+++ src/components/sliders/Home3.js
@@ -130,56 +130,6 @@ export default class Home3Slider extends Component {
                 </div>
               </div>
             </div>
-            {/*=== Single Slider ===*/}
-            <div className="single-slider">
-              <div
-                className="image-layer bg_cover"
-                style={{
-                  backgroundImage:
-                    "url(assets/images/hero/hero-three_img-3.jpg)",
-                }}
-              />
-              <div className="container">
-                <div className="row align-items-center">
-                  <div className="col-xl-7">
-                    {/*=== Hero Content ===*/}
-                    <div className="hero-content text-white">
-                      <h3 className="wow fadeInDown mb-30" data-wow-delay=".5s">
-                        A Co-ExIStic{" "}
-                        <span style={{ color: "#e5af54" }}>
-                          (Collaborative | Experimental | Innovative |
-                          Sustainable | Holistic)
-                        </span>{" "}
-                        Village for a Thriving Lifestyle & LiveLEhood
-                      </h3>
-                      <p className="wow fadeInUp" data-wow-delay=".6s">
-                        Harmony in Diversity: A Co-Existic Village for a
-                        Thriving Lifestyle and Livelihood
-                      </p>
-
-                      <div
-                        className="hero-button"
-                        data-animation="fadeInRight"
-                        data-delay=".6s"
-                      >
-                        <Link legacyBehavior href="/about">
-                          <a className="main-btn primary-btn">
-                            Explore More
-                            <i className="fas fa-paper-plane" />
-                          </a>
-                        </Link>
-                        <Link legacyBehavior href="/about">
-                          <a className="main-btn secondary-btn">
-                            Learn More
-                            <i className="fas fa-paper-plane" />
-                          </a>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </Slider>
         </div>
       </section>
